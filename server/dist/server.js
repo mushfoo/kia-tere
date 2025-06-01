5765979c9ccc8da475ac6499dba2ff84545677a8
Campbell <campbellrehu@me.com>
2025-06-01 22:46:13 +1200

    Add server/dist to gitignore and remove from tracking

--- server/dist/server.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const WebSocketServer_1 = require("./services/WebSocketServer");
-const server = new WebSocketServer_1.WebSocketServer(9191);
-server.start();
-//# sourceMappingURL=server.js.map
--- server/dist/utils/roomUtils.d.ts
+++
@@ -1,6 +0,0 @@
-import { Room, GameState } from '../types';
-export declare function generateRoomCode(): string;
-export declare function createInitialGameState(players: string[]): GameState;
-export declare function shouldCleanupRoom(room: Room): boolean;
-export declare function getRandomCategory(): string;
-//# sourceMappingURL=roomUtils.d.ts.map
